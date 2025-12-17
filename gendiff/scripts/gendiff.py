04dc323a2595a3fc65d0b51d3e583f79794507a8
Byron Ramirez <tauren62@gmail.com>
2025-12-16 21:29:21 -0500

    correction new command and code
    nuevo líneas de código y cambio de archivos

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,7 @@
 import argparse
-import json
 from gendiff import generate_diff
 
 
-def parse_file(filepath):
-    """Lee un archivo JSON."""
-    with open(filepath) as f:
-        return json.load(f)
-    
 def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
@@ -21,18 +15,11 @@ def main():
         help="set format of output",
         metavar="FORMAT"
     )
+
     args = parser.parse_args()
 
-    # Leer los archivos
-    data1 = parse_file(args.first_file)
-    data2 = parse_file(args.second_file)
-
-    # Solo para comprobar que funciona
-    print("Archivo 1 cargado:", data1)
-    print("Archivo 2 cargado:", data2)
-
-    diff = generate_diff(args.first_file, args.second_file)
-    print(diff)
+    result = generate_diff(args.first_file, args.second_file)
+    print(result)
 
 
 if __name__ == "__main__":
